ab287ce4ec568df540bee148e5ee9bd2616ca540
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-06 17:17:01 +0300

    Add files via upload

---
+++ 4. Рамка.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+	system("chcp 1251"); // Установить кодировку
+	int width;
+	int height;
+	std::cout << "Введите ширину и высоту рамки: ";
+	std::cin >> width >> height;
+
+	for (int row = 0; row <= width; row++) {
+		for (int col = 0; col <= height; col++) {
+			if (row == 0) {
+				std::cout << "-";
+			}
+			else if (row == width) {
+				std::cout << "-";
+			}
+			else if (col == 0) {
+				std::cout << "|";
+			}
+			else if (col == height) {
+				std::cout << "|";
+			}
+			else {
+				std::cout << " ";
+			}
+
+		}
+		std::cout << "\n";
+	}
+
+}
+
