b8d666f0e1724b7e7302ae129f51435354a598ee
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-06 17:41:51 +0300

    Add files via upload

---
+++ 5. Координатные оси.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+    for (int x = 0; x <= 20; x++) {
+        for (int y = 0; y <= 50; y++) {
+            if (x == 10 && y == 25) {
+                std::cout << "+";
+            }
+            else if (x == 0 && y == 25) {
+                std::cout << "^";
+            }
+            else if (x == 10 && y == 50) {
+                std::cout << ">";
+            }
+            else if (x == 10) {
+                std::cout << "-";
+            }                
+            else if (y == 25) {
+                std::cout << "|";
+            }
+            else {
+                std::cout << " ";
+            }               
+        }
+        std::cout << "\n";
+    }   
+}
+
