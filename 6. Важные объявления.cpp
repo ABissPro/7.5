d2d14dc43cf5981c5b9257fb9acf451a1b06e001
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-06 21:59:46 +0300

    Add files via upload

---
+++ 6. Важные объявления.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+	system("chcp 1251"); // Установить кодировку
+	int length;
+	int exMark;
+	std::cout << "Введите общую длину колонтитула: ";
+	std::cin >> length;
+	std::cout << "Введите количество восклицательных знаков: ";
+	std::cin >> exMark;
+
+	int rSide = (length - exMark) / 2;
+	int lSide = length - rSide - (length - exMark)% 2;
+	
+	for (int col = 1; col <= length; col++) {
+		//работает для тех, где нужен сдвиг. Где не нужен сдвиг ! на один меньше 
+			if (col <= rSide) {
+				std::cout << "~";
+			}
+			else if (col > lSide) {
+				std::cout << "~";
+			}
+			else {
+				std::cout << "!";
+			}
+		
+		
+		
+	}
+
+}
